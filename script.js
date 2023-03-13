2c0350a9f016c334c13c38b182edbf1ca83f7357
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-03-13 01:13:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+     document.querySelector('.slider').src = anything;
+    }
+    function changeBgColor(color) {
+      const container = document.querySelector('.container');
+      container.style.background = color;
+    }
